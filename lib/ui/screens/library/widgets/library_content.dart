06ce39b2b586f77c51c6684976164bf146f25c52
BN2323 <Bunnaroth.tha@student.cadt.edu.kh>
2026-03-05 11:16:18 +0700

    refactoring library content to read only LibraryViewModel and AppSettingState

--- lib/ui/screens/library/widgets/library_content.dart
+++ lib/ui/screens/library/widgets/library_content.dart
@@ -1,24 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:w7_small_homework/model/songs/song.dart';
+import 'package:provider/provider.dart';
 import 'package:w7_small_homework/ui/screens/library/library_screen.dart';
-import 'package:w7_small_homework/ui/states/player_state.dart';
+import 'package:w7_small_homework/ui/screens/library/view_model/library_view_model.dart';
 import 'package:w7_small_homework/ui/states/settings_state.dart';
 import 'package:w7_small_homework/ui/theme/theme.dart';
 
 class LibraryContent extends StatelessWidget {
-  const LibraryContent({
-    super.key,
-    required this.settingsState,
-    required this.songs,
-    required this.playerState,
-  });
-
-  final AppSettingsState settingsState;
-  final List<Song> songs;
-  final PlayerState playerState;
+  const LibraryContent({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<LibraryViewModel>();
+    final settingsState = context.watch<AppSettingsState>();
+
     return Container(
       color: settingsState.theme.backgroundColor,
       child: Column(
@@ -26,19 +20,21 @@ class LibraryContent extends StatelessWidget {
         children: [
           SizedBox(height: 16),
           Text("Library", style: AppTextStyles.heading),
-    
+
           SizedBox(height: 50),
-    
+
           Expanded(
             child: ListView.builder(
-              itemCount: songs.length,
-              itemBuilder: (context, index) => SongTile(
-                song: songs[index],
-                isPlaying: playerState.currentSong == songs[index],
-                onTap: () {
-                  playerState.start(songs[index]);
-                },
-              ),
+              itemCount: viewModel.songs.length,
+              itemBuilder: (context, index) {
+                final song = viewModel.songs[index];
+
+                return SongTile(
+                  song: song,
+                  isPlaying: viewModel.isPlaying(song),
+                  onTap: () => viewModel.play(song),
+                );
+              },
             ),
           ),
         ],
